c5315eae3e7461d4bc581dcf6bdba06585aef4e4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-09-16 13:13:42 -0500

    [UPDATE]

--- tarea1-2-3/src/components/container/A.jsx
+++ tarea1-2-3/src/components/container/A.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Contacto } from '../../models/contacto.class';
+import PropTypes from 'prop-types';
 import B from '../pure/B';
 
 const A = () => {
@@ -15,6 +16,13 @@ const A = () => {
 			<B contacto = { contacto }></B>
 		</div>
 	);
+}
+
+A.propTypes = {
+	nombre: PropTypes.string,
+	apellido: PropTypes.string,
+	email: PropTypes.string,
+	conectado: PropTypes.bool,
 }
 
 export default A;
